eacb5dbb7a9999867512da90a145934b9660b12e
pio1755 <44492933+pio1755@users.noreply.github.com>
2019-10-28 15:41:59 +0100

    Update countingSort.py
    zmiana xd

--- countingSort.py
+++ countingSort.py
@@ -4,9 +4,7 @@ for x in range(len(wpisanie)):
     lista.append(int(wpisanie[x]))
 print(lista)
 index = []
-ostateczna = []
-for x in range(len(lista)):
-    ostateczna.append(0)
+
 for x in range(0, max(lista)+2,1 ):
     index.append(0)
 print(index)
@@ -16,17 +14,11 @@ for x in range(len(lista)):
         if lista[x] == y:
             index[y]+=1
 print(index)
-for x in range(0,len(index)-1):
-    if x < len(index):
-        index[x+1]=index[x]+index[x+1]
 
-print(index)
+ostateczna =[]
 
-for x in range(0,len(lista)):
-    for y in range(0,):
-        if lista[x] == x and x < len(index):
-            ostateczna[x]=x
+for x in range(len(index)):
+    for y in range(index[x]):
+        ostateczna.append(x)
 
 print(ostateczna)
-
-
